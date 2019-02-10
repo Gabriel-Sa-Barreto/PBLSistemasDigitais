e70c6c2cb9b411de1e60e1c5cf55c55306cb76cf
Samuel Vitorio Lima <samvlima10@gmail.com>
2019-02-10 15:18:18 -0200

    Insercao do clock counter

--- arbitro/arbitro.v
+++ arbitro/arbitro.v
@@ -7,23 +7,26 @@ module arbitro (
 	output reg         tx;	
 );
 
-parameter IDLE         = 4'b0000;
-parameter REQUEST      = 4'b0001;
-parameter ANSWER_TIME  = 4'b0010;
-parameter TIMEOUT      = 4'b0011;
-parameter CHECKSUM     = 4'b0100;
-parameter SEND_BACK    = 4'b0101;
-parameter TRYALARM     = 4'b0110;
-parameter ALARM        = 4'b0111;
-parameter CLEAN_BUFFER = 4'b1000;
+parameter IDLE         = 3'b000;
+parameter REQUEST      = 3'b001;
+parameter ANSWER_TIME  = 3'b010;
+parameter TIMEOUT      = 3'b011;
+parameter CHECKSUM     = 3'b100;
+parameter SEND_BACK    = 3'b101;
+parameter TRYALARM     = 4'b110;
+//parameter ALARM        = 4'b0111;
+//parameter CLEAN_BUFFER = 4'b1000;
 
 parameter c_CLOCK_PERIOD_NS = 100;
 parameter c_CLKS_PER_BIT    = 87; //parâmetro do tx para o clock por bit
 reg wire  r_Tx_DV = 0;            //informa se pode transmitir algo via TX 
+
 wire w_Tx_Done;                   //informa que a transmissão foi concluída
 wire [15:0] w_Rx_Byte;
 wire rx_DV;
+wire done;                        //informa se os 10 segundos do tempo de resposta foi finalizado
 
+reg active;                       //ativa a contagem de 10 segundos
 reg r_Rx_Serial = 1;
 reg r_Clock = 0;
 reg [2:0] state;
@@ -39,69 +42,104 @@ always
 
 always @ (clock)
 	begin
-		if(reset){
+		if(reset)
 			state <= IDLE;
-		}else{
+		else
 			state <= next_state;
-		}
 	end
 
 always @ (state,dataA)
 	begin
 		case(state)
-			IDLE:
-				/*Se dataA tiver algum valor, então muda para o estado Timer para envio de uma requisição*/
-				if(dataA){
+			begin
+				IDLE:
 					begin
-						requestSensor  <= dataA[7:0]; //sensor requisitado
-						       r_Tx_DV <= 1'b1;
-						next_state     <= REQUEST;
+						/*Se dataA tiver algum valor, então muda para o estado Timer para envio de uma requisição*/
+						if(dataA)
+							begin
+								requestSensor  <= dataA[7:0]; //sensor requisitado
+										 r_Tx_DV <= 1'b1;
+								next_state     <= REQUEST;
+							end
+						else
+							begin
+								next_state <= IDLE;
+							end
 					end
-				}else
+					
+				REQUEST: /*Espera fim da requisição*/
 					begin
-						next_state <= IDLE;
+						//espera a finalização pelo w_Tx_Done e após isso dá um time de resposta
+						if(w_Tx_Done)
+							begin
+								requestSensor <= 0;
+										r_Tx_DV <= 1'b0;
+										 active <= 1'b1; //ativa a contagem
+								next_state    <= ANSWER_TIME;
+							end
+						else
+							begin
+								next_state    <= REQUEST
+							end	
+					end			
+					
+				ANSWER_TIME:
+					/*Espera 10 segundos, caso nenhuma resposta tenha chegado via RX, próximo estado será TIMEOUT*/
+					begin
+						if(done)
+							begin
+									 active   <= 1'b0; //desativa a contagem
+								next_state   <= TIMEOUT;
+							end
+						else
+							begin
+								if(rx_DV) //verifica se o RX recebeu algum pacote
+									begin
+										recivedValue <= w_Rx_Byte;
+										next_state   <= CHECKSUM;
+									end
+							end
 					end
-
-			REQUEST: /*Espera fim da requisição*/
-				//espera a finalização pelo w_Tx_Done e após isso dá um time de resposta
-				if(w_Tx_Done){
-					begin
-						requestSensor <= 0;
-								r_Tx_DV <= 1'b0;
-						next_state    <= ANSWER_TIME;
-					end
-				}else{
-					begin
-						next_state    <= REQUEST
-					end				
-				}
 				
-			ANSWER_TIME:
-				/*Espera 10 segundos, caso nenhuma resposta tenha chegado via RX, próximo estado será TIMEOUT*/
+				TIMEOUT: 
+					next_state <= IDLE;
 			
-			TIMEOUT:
-			   next_state <= IDLE;
-		
-			CHECKSUM:
-				comparePack = checkSum ^ receivedValue[7:0]
-				if( comparePack == receiveValue[15:8]){
+				CHECKSUM:
 					begin
-						//retorna o resultado para o software em C
+						comparePack = checkSum ^ receivedValue[7:0];
+						if( comparePack == receiveValue[15:8])
+							begin
+								//retorna o resultado para o software em C
+								next_statE <= SEND_BACK;
+							end
+						else
+							begin
+									 active <= 1'b1; //ativa a contagem
+								next_state <= TRYALARM;
+							end
+					end			
+			
+				SEND_BACK:
+					//colocar na memoria
+
+				TRYALARM:
+					/*Espera um tempo pra saber se recebe mais algo pacote, se receber é um alarme, senão, foi choque de dados*/
+					begin
+						if(done)
+							begin
+									    active   <= 1'b0; //desativa a contagem
+									next_state   <= IDLE;
+							end
+						else
+							begin
+								if(rx_DV) //verifica se o RX recebeu algum pacote
+									begin
+										recivedValue <= w_Rx_Byte;
+										next_state   <= CHECKSUM;
+									end
+							end
 					end
-				}else{
-					next_state <= TRYALARM;
-				}
-			
-			
-			SEND_BACK:
-
-			TRYALARM:
-				/*Espera um tempo pra saber se recebe mais algo pacote, se receber é um alarme, senão, foi choque de dados*/
-			
-			
-			ALARM:
-
-			CLEAN_BUFFER:
+			end
 		endcase
 	end
 
@@ -126,6 +164,13 @@ uart_rx uart_rx_inst
 	.o_Rx_Byte(w_Rx_Byte) 					// output [7:0] o_Rx_Byte_sig
 );
 
+
+digital_clock digital_clock_inst
+(
+	.clk(clock) ,								// input  clk_sig
+	.active(active) ,							// input  active_sig
+	.done(done) 								// output  done_sig
+);
 
 
 
